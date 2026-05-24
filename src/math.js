8a254aa80447b42c31b90dc4dcab41f674fc414b
Fernando Pascoal <contato.fernandopascoal@gmail.com>
2026-05-24 19:06:56 -0300

    add matrix versions (#4)
    * add matrix versions
    
    * ci: add dockerfile
    
    * ci: add docker build
    
    * fix: node version ci.yml
    
    * ci: config sonar
    
    * ci: fix organization
    
    * ci: fix dockerhub tags
    
    * ci: generate coverage
    
    * test: increment test
    
    * ci: fix test command
    
    * ci: await sonarqube
    
    * ci: fix test coverage properties sonar

--- src/math.js
+++ src/math.js
@@ -1,5 +1,13 @@
 function sum(n1, n2) {
+  if (true) {
+    console.log(n1 * n2 + 4242);
+  }
+
   return n1 + n2;
 }
 
-module.exports = { sum };
+function mult(n1, n2) {
+  return n1 * n2
+}
+
+module.exports = { sum, mult };
