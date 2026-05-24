af1157da48e9f0b230fdb40e60e5260f1426142a
Fernando Pascoal <contato.fernandopascoal@gmail.com>
2026-05-24 11:35:41 -0300

    ci: config sonar

--- src/math.js
+++ src/math.js
@@ -1,5 +1,17 @@
 function sum(n1, n2) {
+  if (false) {
+    console.log(n1 * n2);
+  }
+
+  if (true) {
+    console.log(n1 * n2 + 4242);
+  }
+
   return n1 + n2;
 }
 
-module.exports = { sum };
+function mult(n1, n2) {
+  return n1 * n2
+}
+
+module.exports = { sum, mult };
